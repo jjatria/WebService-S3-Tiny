44dff579c6e4f69018c0dba4ede739fda8086588
James Raspass <jraspass@gmail.com>
2018-03-12 11:48:53 +0000

    Fold sign_request into _request

--- lib/WebService/S3/Tiny.pm
+++ lib/WebService/S3/Tiny.pm
@@ -63,58 +63,6 @@ sub get_object {
     $self->_request( 'GET', $bucket, $object );
 }
 
-sub sign_request {
-    my ( $self, $method, $path, $query, $headers, $content ) = @_;
-
-    my $date = substr my $time = $headers->{'x-amz-date'}, 0, 8;
-
-    $path = _normalize_path($path);
-
-    # FIXME Quick fix for the aws.t
-    $path =~ s/ /%20/;
-
-    my $creq = "$method\n$path\n" . HTTP::Tiny->www_form_urlencode($query);
-
-    for my $k ( sort keys %$headers ) {
-        my $v = $headers->{$k};
-
-        $creq .= "\n$k:";
-
-        $creq .= join ',',
-            map s/\s+/ /gr,
-            map s/^\s+|\s+$//gr,
-            map split(/\n/),
-            ref $v ? @$v : $v;
-    }
-
-    $creq .= "\n\n";
-
-    $creq .= my $signed_headers = join ';', sort keys %$headers;
-
-    $creq .= "\n" . ( $headers->{'x-amz-content-sha256'} // sha256_hex $content );
-
-    my $cred_scope = "$date/$self->{region}/$self->{service}/aws4_request";
-
-    my $sig = hmac_sha256_hex(
-        "AWS4-HMAC-SHA256\n$time\n$cred_scope\n" . sha256_hex($creq),
-        hmac_sha256(
-            aws4_request => hmac_sha256(
-                $self->{service} => hmac_sha256(
-                    $self->{region},
-                    hmac_sha256( $date, "AWS4$self->{secret_key}" ),
-                ),
-            ),
-        ),
-    );
-
-    join(
-        ', ',
-        "AWS4-HMAC-SHA256 Credential=$self->{access_key}/$cred_scope",
-        "SignedHeaders=$signed_headers",
-        "Signature=$sig",
-    );
-}
-
 sub _normalize_path {
     my @old_parts = split m(/), $_[0], -1;
     my @new_parts;
@@ -136,41 +84,83 @@ sub _normalize_path {
 sub _request {
     my ( $self, $method, $bucket, $object, $content, $query, $headers ) = @_;
 
+    $content //= '';
+    $headers //= {};
+    $query   //= {};
+
     my $path = "/$bucket";
 
     $path .= "/$object" if defined $object;
 
-    $content //= '';
+    $path = _normalize_path($path);
 
-    my %headers = %{ $headers // {} };
+    # FIXME Quick fix for the aws.t
+    $path =~ s/ /%20/;
 
-    $headers{host} = $self->{host} =~ s|^https?://||r;
+    $headers->{host} = $self->{host} =~ s|^https?://||r;
 
     my ( $s, $m, $h, $d, $M, $y ) = gmtime;
 
-    $headers{'x-amz-date'} = sprintf '%d%02d%02dT%02d%02d%02dZ',
+    my $time = $headers->{'x-amz-date'} = sprintf '%d%02d%02dT%02d%02d%02dZ',
         $y + 1900, $M + 1, $d, $h, $m, $s;
 
-    # Let the user pass their own checksums if they have them.
-    $headers{'x-amz-content-sha256'} //= sha256_hex $content
-        if $method eq 'PUT';
+    my $date = substr $time, 0, 8;
 
-    $headers{authorization} = $self->sign_request(
-        $method,
-        $path,
-        $query //= {},
-        \%headers,
-        $content,
+    # Let the user pass their own checksums if they have them.
+    my $sha = $headers->{'x-amz-content-sha256'} // sha256_hex $content;
+
+    # Put requests must have the checksum header.
+    $headers->{'x-amz-content-sha256'} = $sha if $method eq 'PUT';
+
+    my $creq = "$method\n$path\n" . HTTP::Tiny->www_form_urlencode($query);
+
+    for my $k ( sort keys %$headers ) {
+        my $v = $headers->{$k};
+
+        $creq .= "\n$k:";
+
+        $creq .= join ',',
+            map s/\s+/ /gr,
+            map s/^\s+|\s+$//gr,
+            map split(/\n/),
+            ref $v ? @$v : $v;
+    }
+
+    $creq .= "\n\n";
+
+    $creq .= my $signed_headers = join ';', sort keys %$headers;
+
+    $creq .= "\n$sha";
+
+    my $cred_scope = "$date/$self->{region}/$self->{service}/aws4_request";
+
+    my $sig = hmac_sha256_hex(
+        "AWS4-HMAC-SHA256\n$time\n$cred_scope\n" . sha256_hex($creq),
+        hmac_sha256(
+            aws4_request => hmac_sha256(
+                $self->{service} => hmac_sha256(
+                    $self->{region},
+                    hmac_sha256( $date, "AWS4$self->{secret_key}" ),
+                ),
+            ),
+        ),
+    );
+
+    $headers->{authorization} = join(
+        ', ',
+        "AWS4-HMAC-SHA256 Credential=$self->{access_key}/$cred_scope",
+        "SignedHeaders=$signed_headers",
+        "Signature=$sig",
     );
 
     # HTTP::Tiny doesn't like us providing our own host header, but we have to
     # sign it, so let's hope HTTP::Tiny calculates the same value as us :-S
-    delete $headers{host};
+    delete $headers->{host};
 
     $self->{ua}->request(
         $method,
         "$self->{host}$path?" . HTTP::Tiny->www_form_urlencode($query),
-        { content => $content, headers => \%headers },
+        { content => $content, headers => $headers },
     );
 }
 
